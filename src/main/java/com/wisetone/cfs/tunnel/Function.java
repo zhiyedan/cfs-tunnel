dec9d7438a9868ff57047d7b0e4499fcee1edf25
zhiyedan <shijia0620@126.com>
2017-05-09 17:12:20 +0800

    add ipsec of gre tunnel function

--- src/main/java/com/wisetone/cfs/tunnel/Function.java
+++ src/main/java/com/wisetone/cfs/tunnel/Function.java
@@ -8,8 +8,12 @@ import org.json.JSONObject;
  */
 public class Function {
 
-    public void getTopo(){
-        String url = "http://192.168.0.110:8181/restconf/operational/network-topology:network-topology/";
+    public static String odlIP = "http://192.168.0.110:8181/";
+    /*
+    * 获取整个网络的拓扑结构--截止到node层
+    * */
+    public static void getTopo(){
+        String url = odlIP+"restconf/operational/network-topology:network-topology/";
         String result = ODLRestUtil.getUtil(url);
         JSONObject jsonObject = new JSONObject(result);
         JSONObject netTopo = (JSONObject) jsonObject.get("network-topology");
@@ -28,5 +32,33 @@ public class Function {
                 }
             }
         }
+    }
+    /*
+    * 添加 Ipsec-GRE tunnel port
+     *
+    * */
+    public static void addTunnelPort(String nodeId,String remoteIP){
+        String nodeIdInURL = nodeId.replaceAll("/","%2F");
+        String url = odlIP+"restconf/config/network-topology:network-topology/topology/ovsdb:1/node/"+nodeIdInURL+"/termination-point/ipsec-gre/";
+        JSONObject content = new JSONObject();
+        JSONArray terminalPoints = new JSONArray();
+        JSONObject terminalPoint = new JSONObject();
+        terminalPoint.put("ovsdb:name","ipsec-gre");
+        terminalPoint.put("ovsdb:interface-type","ovsdb:interface-type-ipsec-gre");
+        terminalPoint.put("tp-id","ipsec-gre");
+        JSONArray options = new JSONArray();
+        JSONObject option0 = new JSONObject();
+        option0.put("ovsdb:option","remote_ip");
+        option0.put("ovsdb:value",remoteIP);
+        JSONObject option1 = new JSONObject();
+        option1.put("ovsdb:option","psk");
+        option1.put("ovsdb:value","test123");
+        options.put(0,option0);
+        options.put(1,option1);
+        terminalPoint.put("ovsdb:options",options);
+        terminalPoints.put(0,terminalPoint);
+        content.put("network-topology:termination-point",terminalPoints);
+        System.out.println(content.toString());
+        ODLRestUtil.putUtil(url,content);
     }
 }
--- src/main/java/com/wisetone/cfs/tunnel/Main.java
+++ src/main/java/com/wisetone/cfs/tunnel/Main.java
@@ -6,6 +6,7 @@ package com.wisetone.cfs.tunnel;
 public class Main {
     public static void main(String[] args) {
 //        System.out.println(B.getX());
-
+//        Function.getTopo();
+        Function.addTunnelPort("ovsdb://mini2/bridge/s1","192.168.0.109");
     }
 }
